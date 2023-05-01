c1991affa788bf2845593e87898ff07666e1b83e
JeremyLapham <jeremylapham2004@gmail.com>
2023-05-01 12:15:32 -0700

    basic login skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import Login from './Components/loginComponent/Login';
+import CreateAccount from './Components/CreateAccount/CreateAccount';
 
 function App() {
   return (
-    <Container>
+    <Container fluid>
       <Row>
-
-
+        <Col>
+        <Login />
+        <CreateAccount />
+        </Col>
       </Row>
     </Container>
   );
--- src/Components/loginComponent/Login.tsx
+++ src/Components/loginComponent/Login.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        let userData = {
+            userName,
+            password
+        }
+    }
+    return (
+        <Container fluid>
+            <Row>
+                <Col>
+                    <input onChange={({ target: { value } }) => setUserName(value)} value={userName} placeholder='Enter your username' />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <input onChange={({ target: { value } }) => setPassword(value)} value={password} placeholder='Enter your password' />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button onClick={handleLogin}>Log In</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
