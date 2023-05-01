7e9e6823e0e16400dd59e120d96cba975cc54bc9
JeremyLapham <jeremylapham2004@gmail.com>
2023-05-01 10:13:01 -0700

    file converting

---
+++ src/Components/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CreateAccount.css'
+
+export default function CreateAccount() {
+  return (
+    <div>CreateAccount</div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
+        <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
     <App />
   </React.StrictMode>
 );
@@ -14,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
