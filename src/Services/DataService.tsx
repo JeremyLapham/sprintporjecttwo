dbb4c827c2d8ea8f9d31602fc5959253d2ab63fa
ricRobo4 <artistr3@proton.me>
2023-05-03 10:10:07 -0700

    2 Changes: Hover to view * Changed variable names *Added function and variable for Kata data fetch

--- src/Services/DataService.tsx
+++ src/Services/DataService.tsx
@@ -1,36 +1,54 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 
-let codeWarsData: any = '';
+let codeWarsUserData: any = '';
+let codeWarsKataData: any = '';
 
 async function UserAPIFetch ()
 {
     let res = await fetch(`https://www.codewars.com/api/v1/users/ricRobo4`);
     let data = await res.json();
-    codeWarsData = data;
-    // console.log(data.id);
-    // console.log(data.username);
-    // console.log(data.name);
-    // console.log(data.honor);
-    console.log(codeWarsData);
+    codeWarsUserData = data;
+    console.log(codeWarsUserData);
+    console.log('Rank: ' + codeWarsUserData.ranks.overall.rank);
+    console.log('Username: ' + codeWarsUserData.username);
+    console.log('Name: ' + codeWarsUserData.name);
+    console.log('Honor: ' + codeWarsUserData.honor);
+    console.log('Clan: ' + codeWarsUserData.clan);
+    console.log('LeaderBoard Position: ' + codeWarsUserData.leaderboardPosition);
+    console.log('Total Authored Katas: ' + codeWarsUserData.codeChallenges.totalAuthored);
+    console.log('Total Completed Katas: ' + codeWarsUserData.codeChallenges.totalCompleted);
 }
 
-function Test () {
+async function KataAPIFetch ()
+{
+    let res = await fetch(`https://www.codewars.com/api/v1/code-challenges/5556282156230d0e5e000089`);
+    let data = await res.json();
+    codeWarsKataData = data;
+    console.log(codeWarsKataData);
+    console.log(codeWarsKataData.rank.name);
+    console.log('Title: ' + codeWarsKataData.name);
+    console.log('Author: ' + codeWarsKataData.createdBy.username);
+    console.log('Languages Available: ' + codeWarsKataData.languages);
+    console.log('Description: ' + codeWarsKataData.description);
+}
 
-    return(
-        <Container>
-            <Row>
-                <Col>
-                    <button onClick={UserAPIFetch}>Get API Data</button>     
-                </Col>
-            </Row>
-            <Row>
-                <Col>
+// function Test () {
+
+//     return(
+//         <Container>
+//             <Row>
+//                 <Col>
+//                     <button onClick={UserAPIFetch}>Get API Data</button>     
+//                 </Col>
+//             </Row>
+//             <Row>
+//                 <Col>
                     
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+//                 </Col>
+//             </Row>
+//         </Container>
+//     );
+// }
 
-export { Test, UserAPIFetch }
+export { UserAPIFetch, KataAPIFetch }
