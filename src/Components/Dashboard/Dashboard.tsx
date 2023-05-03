8847891dd8a6c75ff1579b734aa561ff7e0e65b4
JeremyLapham <jeremylapham2004@gmail.com>
2023-05-03 12:13:36 -0700

    pulling data

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import './Dashboard.css';
-import TopUser from '../TopUser/TopUser'
+import TopUser from '../TopUser/TopUser';
+import { GetKataData } from '../../Services/DataService';
+import { MyContext } from '../context';
+
 export default function Dashboard() {
+  const { username } = useContext(MyContext);
+
+  useEffect(() => {
+    const UsersKataData = async () => {
+      const usersData = await GetKataData(username);
+      console.log(usersData);
+    };
+    UsersKataData()
+  }, []);
   return (
     <div>
-    <TopUser />
+      <TopUser />
     </div>
   )
 }
