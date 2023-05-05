b0496a92375e81054f673685ac7f3215de1fd8aa
Elizar Garcia <garciaelizar@yahoo.com>
2023-05-05 11:08:55 -0700

    update

--- src/Components/UserDrop/UserDrop.tsx
+++ src/Components/UserDrop/UserDrop.tsx
@@ -5,7 +5,8 @@ import "./Userdrop.css";
 
 
 
-function UserDrop (props) {
+function UserDrop (props: { userData: {
+  ranks: { overall: { name:string; color:string}}; codeChallenges: any; username: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; clan: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; honor: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; leaderboardPosition: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }; }) {
   return (
     <div>
       <Row className="infoDivTwo">
@@ -21,16 +22,19 @@ function UserDrop (props) {
         <Col></Col>
         <Col className="dBarOne">
 
+          {props.userData.map((userData) =>(
+            <div>{userData}</div>
+          ))}
           <Accordion defaultActiveKey="0">
 
             <Accordion.Item eventKey="1">
-              <Accordion.Header>{props.kyu} {props.name}</Accordion.Header>
+              <Accordion.Header className={`${props.userData.ranks.overall.name}`}> {props.userData.ranks.overall.name}</Accordion.Header>
               <Accordion.Body className="dBox">
                 <div>
-                  <div>Honor:{props.honor}</div>
-                  <div>Leaderboard:{props.leaderboard}</div>
-                  <div>Katas Completed:{props.completedKatas}</div>
-                  <div>Katas Authored:{props.authoredKatas}</div>
+                  <div>{props.userData.honor}</div>
+                  <div>Leaderboard:{props.userData.leaderboardPosition}</div>
+                  <div>Katas Completed:{props.userData.codeChallenges.totalCompleted}</div>
+                  <div>Katas Authored:{props.userData.codeChallenges.totalAuthored}</div>
                 </div>
                 <div>
                   <Button>Reserve Kata</Button>
