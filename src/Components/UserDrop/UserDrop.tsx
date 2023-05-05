cfe44abb850b79f051d90cec4aafa7b2bd7204c7
Elizar Garcia <garciaelizar@yahoo.com>
2023-05-05 10:44:16 -0700

    update

--- src/Components/UserDrop/UserDrop.tsx
+++ src/Components/UserDrop/UserDrop.tsx
@@ -1,112 +1,49 @@
-import React from 'react'
-import { Accordion, Col, Row,Button } from 'react-bootstrap'
-import './Userdrop.css';
+import React from "react";
+import { Accordion, Col, Row, Button } from "react-bootstrap";
+import "./Userdrop.css";
 
-const UserDrop = () => {
+
+
+
+function UserDrop (props) {
   return (
     <div>
-          <Row className='infoDivTwo'>
-      <Col>
-      <p className='rChall' style={{color: 'white'}}>Random Challenge</p>
+      <Row className="infoDivTwo">
+        <Col>
+          <p className="rChall" style={{ color: "white" }}>
+            Random Challenge
+          </p>
+        </Col>
+        <Col></Col>
+      </Row>
 
-      </Col>
-      <Col></Col>
-    </Row>
+      <Row>
+        <Col></Col>
+        <Col className="dBarOne">
 
-    <Row >
-      <Col></Col>
-      <Col className='dBarOne' >
+          <Accordion defaultActiveKey="0">
 
-      <Accordion  defaultActiveKey="0">
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>6kyu UserName</Accordion.Header>
-        <Accordion.Body className='dBox'>
-          <div >
-           <div >Honor:174</div >
-       <div >Leaderboard Position:#365,751</div >
-       <div >Katas Completed:12</div >
-       <div >Katas Authored:2</div > 
-          </div>
-          <div>
-            <Button>Reserve Kata</Button>
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>{props.kyu} {props.name}</Accordion.Header>
+              <Accordion.Body className="dBox">
+                <div>
+                  <div>Honor:{props.honor}</div>
+                  <div>Leaderboard:{props.leaderboard}</div>
+                  <div>Katas Completed:{props.completedKatas}</div>
+                  <div>Katas Authored:{props.authoredKatas}</div>
+                </div>
+                <div>
+                  <Button>Reserve Kata</Button>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
 
-    <Accordion  defaultActiveKey="0">
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>7kyu UserName</Accordion.Header>
-        <Accordion.Body className='dBox'>
-          <div >
-           <div >Honor:174</div >
-       <div >Leaderboard Position:#365,751</div >
-       <div >Katas Completed:12</div >
-       <div >Katas Authored:2</div > 
-          </div>
-          <div>
-            <Button>Reserve Kata</Button>
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+          </Accordion>
 
-    <Accordion  defaultActiveKey="0">
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>4kyu UserName</Accordion.Header>
-        <Accordion.Body className='dBox'>
-          <div >
-           <div >Honor:174</div >
-       <div >Leaderboard Position:#365,751</div >
-       <div >Katas Completed:12</div >
-       <div >Katas Authored:2</div > 
-          </div>
-          <div>
-            <Button>Reserve Kata</Button>
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-
-    <Accordion  defaultActiveKey="0">
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>7kyu UserName</Accordion.Header>
-        <Accordion.Body className='dBox'>
-          <div >
-           <div >Honor:174</div >
-       <div >Leaderboard Position:#365,751</div >
-       <div >Katas Completed:12</div >
-       <div >Katas Authored:2</div > 
-          </div>
-          <div>
-            <Button>Reserve Kata</Button>
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-
-    <Accordion  defaultActiveKey="0">
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>2kyu UserName</Accordion.Header>
-        <Accordion.Body className='dBox'>
-          <div >
-           <div >Honor:174</div >
-       <div >Leaderboard Position:#365,751</div >
-       <div >Katas Completed:12</div >
-       <div >Katas Authored:2</div > 
-          </div>
-          <div>
-            <Button>Reserve Kata</Button>
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-
-            </Col>
-    </Row>
-
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default UserDrop
+export default UserDrop;
